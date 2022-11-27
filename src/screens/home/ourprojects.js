cc3664b75f37a5e966b9b8c6d669d6758b0afac5
Jasham <jasham009@gmail.com>
2022-11-27 15:28:42 +0530

    Update ourprojects.js

--- src/screens/home/ourprojects.js
+++ src/screens/home/ourprojects.js
@@ -313,7 +313,7 @@ const OurProject = (props) => {
                             <div style={{ display : 'flex' ,justifyContent : "center", flexDirection : "column"}}>
 
                                 <CText 
-                                    label="Alenn Watson"
+                                    label="Mustafa"
                                     ffamily={fontFamily('regular')}
                                     fsize={fSize('small')}
                                     color={theme.white}
@@ -345,7 +345,7 @@ const OurProject = (props) => {
                             <div style={{ display : 'flex' ,justifyContent : "center", flexDirection : "column"}}>
 
                                 <CText 
-                                    label="Alenn Watson"
+                                    label="Rohit"
                                     ffamily={fontFamily('regular')}
                                     fsize={fSize('small')}
                                     color={theme.white}
@@ -401,38 +401,7 @@ const OurProject = (props) => {
                             </div>
                             
                         </BlockTwo>
-                        <BlockTwo>
-                            <ImgWOCS>
-                                
-                            </ImgWOCS>
-                            
-                            <div style={{ display : 'flex' ,justifyContent : "center", flexDirection : "column"}}>
-
-                                <CText 
-                                    label="Alenn Watson"
-                                    ffamily={fontFamily('regular')}
-                                    fsize={fSize('small')}
-                                    color={theme.white}
-                                />
-                            </div>
-                            
-                            <CText 
-                                label="UI/UX Designer"
-                                ffamily={fontFamily('light')}
-                                fsize={fSize('12px')}
-                                color={theme.maincolor}
-                            />
-                            <div style={{ width : "70%"}}>
-
-                                <CText 
-                                    label="Service & Support: Before the launch is after the launch! We won't leave you"
-                                    ffamily={fontFamily('regular')}
-                                    fsize={fSize('12px')}
-                                    color={theme.gray2}
-                                />
-                            </div>
-                            
-                        </BlockTwo>
+                        
                     </CusCarousel>
                 </PM235>  
             </WOCWrapper>
